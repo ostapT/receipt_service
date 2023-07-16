d90ba270a85cf79be48ad7ab86c7db048b08d096
ostapix@gmail.com <@Bandera345>
2023-07-16 16:01:27 +0300

    Fixed minor things

--- receipt_service/urls.py
+++ receipt_service/urls.py
@@ -18,12 +18,34 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# Отримайте представлення схеми Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("receipt.urls", namespace="receipt")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path('swagger.yaml/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(
         "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
